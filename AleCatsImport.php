ddfa170576209461a4528c996a9a4bb1046a2b76
alex85 <blyakher85@gmail.com>
2025-03-27 09:35:57 +0200

    remove xml_id old logic

--- AleCatsImport.php
+++ AleCatsImport.php
@@ -5,17 +5,21 @@ class AleCatsImport
   function import($categories_array)
   {
     $imported_terms = array();
+
     foreach ($categories_array as $category_data) {
       if (!isset($imported_terms[$category_data['id']])) {
         $parent_term_id = 0;
 
+        // Если есть родитель и он уже импортирован - используем его ID
         if (!empty($category_data['parent_id']) && isset($imported_terms[$category_data['parent_id']])) {
           $parent_term_id = $imported_terms[$category_data['parent_id']];
         }
 
+        // Проверяем, существует ли термин
         $existing_term = term_exists($category_data['name'], 'product_cat', $parent_term_id);
 
-        if ($existing_term === 0 || $existing_term === null) {
+        if (!$existing_term) {
+          // Создаём новую категорию
           $term_data = wp_insert_term(
             $category_data['name'],
             'product_cat',
@@ -24,15 +28,19 @@ class AleCatsImport
 
           if (!is_wp_error($term_data)) {
             $imported_terms[$category_data['id']] = $term_data['term_id'];
-            add_term_meta($term_data['term_id'], 'xml_id', $category_data['id'], true);
           }
         } else {
+          // Обновляем родителя существующей категории
           $imported_terms[$category_data['id']] = $existing_term['term_id'];
-          wp_update_term($existing_term['term_id'], 'product_cat', array('parent' => $parent_term_id));
-          update_term_meta($existing_term['term_id'], 'xml_id', $category_data['id']);
+          wp_update_term(
+            $existing_term['term_id'],
+            'product_cat',
+            array('parent' => $parent_term_id)
+          );
         }
       }
     }
+
     return $imported_terms;
   }
 }
